72fbae81d3c34e746fc9823102b4109476b247c8
Stephen Fletcher <ste.fletcher@gmail.com>
2018-12-27 14:05:52 +0000

    Initial Post structure and props.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-
 class Post extends React.Component {
     render() {
         return (
-            <div>
-                <p>Post Component</p>
+            <div style={containerStyle} height={this.props.height}>
+            <h3>{this.props.title}</h3>
+            <img 
+                src={this.props.imageSrc}
+                alt={this.props.title} />
+     <p>
+         {this.props.description} 
+     </p>
             </div>
             );
         }
     }
 
+const containerStyle = {
+    backgroundColor: '#00FFCF'
+};
+    
 export default Post;
