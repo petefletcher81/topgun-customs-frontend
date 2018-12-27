c705b92cc4431a1c2f3eaac0244542b67f5dd459
Stephen Fletcher <ste.fletcher@gmail.com>
2018-12-27 13:23:06 +0000

    Initial post component.

---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Post extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Post Component</p>
+            </div>
+            );
+        }
+    }
+
+export default Post;
