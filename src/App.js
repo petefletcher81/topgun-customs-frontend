95b29912d624c32f67ccb9f4647632dcb194fc5e
Pete Fletcher <petefletcher@mbp0012-2.local>
2018-12-27 19:43:01 +0000

    initial styling to Post complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
-import Post from './components/Post';
-// import postData from './data/posts.json'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Post from "./components/Post";
+import postData from "./data/post.json";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    posts: ["post1", "post2", "post3"]
+  };
   render() {
+    console.log("hello");
     return (
-      <div className="App">
-        <Post />
+      <div className="mainLayout">
+        <div className="App">
+          {postData.map(singlePost => {
+            return (
+              <Post
+                imageSrc={singlePost.img}
+                title={singlePost.hero}
+                description="this will be a desc"
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./Post.css";
+
 class Post extends React.Component {
-    render() {
-        return (
-            <div style={containerStyle} height={this.props.height}>
-            <h3>{this.props.title}</h3>
-            <img 
-                src={this.props.imageSrc}
-                alt={this.props.title} />
-     <p>
-         {this.props.description} 
-     </p>
-            </div>
-            );
-        }
-    }
+  render() {
+    return (
+      <div className="postContainer" height={this.props.height}>
+        <h3 className="postHeader">{this.props.title}</h3>
+        <img src={this.props.imageSrc} alt={this.props.title} />
+        <p classname="descPara">{this.props.description}</p>
+      </div>
+    );
+  }
+}
 
 const containerStyle = {
-    backgroundColor: '#00FFCF'
+  backgroundColor: "#00FFCF"
 };
-    
+
 export default Post;
